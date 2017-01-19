feb543d82b96e7272bdec0114ffe69640bdee9fd
Junho <junho.bak@gmail.com>
2017-01-19 13:32:55 +0900

    initial commit

---
+++ DataTemplate/DataTemplate/CarList.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTemplate
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CarList : Window
+    {
+        public void Car(string comp, string name)
+        {
+            this.Company = comp;
+            this.CarName = name;
+        }
+
+        string company = string.Empty;
+        public string Company
+        {
+            get { return company; }
+            set
+            {
+                company = value;
+            }
+        }
+
+        string carName = string.Empty;
+        public string CarName
+        {
+            get { return carName; }
+            set
+            {
+                carName = value;
+            }
+        }
+    }
+
+    public class Cars : ObservableCollection<CarList> { }
+}
---
+++ WpfDataBinding/WpfDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDataBinding
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void insertButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class NameAdds : ObservableCollection<NameAdd> {    }
+
+    public class NameAdd : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Notify(string strName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(strName));
+            }
+        }
+
+        string name = string.Empty;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                Notify("Name");
+            }
+        }
+
+        string add = string.Empty;
+        public string Add
+        {
+            get { return add; }
+            set
+            {
+                add = value;
+                Notify("Add");
+            }
+        }
+
+        public NameAdd() : this("이름입력", "주소입력") { }
+        public NameAdd(string name, string add)
+        {
+            this.name = name;
+            this.add = add;
+        }
+    }
+}
