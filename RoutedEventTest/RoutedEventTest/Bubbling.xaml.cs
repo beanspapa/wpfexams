394de23916f713ab1a8ea70210a49f913d9c035e
Junho <junho.bak@gmail.com>
2017-01-24 20:03:39 +0900

    2017-01-24

---
+++ RoutedEventTest/RoutedEventTest/Bubbling.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEventTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowBubbling.MouseRightButtonDown += new MouseButtonEventHandler(MainWindow_MouseRightButtonDown);
+        }
+
+        private void MainWindow_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Window_MouseRightButtonDown 이벤트 발생");
+        }
+
+        private void btnPuckMan_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "#" + " Sender: " + sender.ToString() + "\r\n" + " Source: " + e.Source + "\r\n" +
+                " Original Source: " + e.OriginalSource;
+            lstMessages.Items.Add(message);
+            lstMessages.Items.Add("버튼이 클릭 되었습니다." + "\r\n");
+        }
+
+        private void SomethingClicked(object sender, MouseButtonEventArgs e)
+        {
+            string message = "#" + "Sender: " + sender.ToString() + "\r\n" +
+                " Source:" + e.Source + "\r\n" +
+                " Original Source: " + e.OriginalSource;
+            lstMessages.Items.Add(message);
+            lstMessages.Items.Add("팩맨 이미지가 클릭되었습니다." + e.Handled + "\r\n");
+            e.Handled = true;
+        }
+    }
+}
---
+++ RoutedEventTest/RoutedEventTest/Tunneling.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoutedEventTest
+{
+    /// <summary>
+    /// Tunneling.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Tunneling : Window
+    {
+        public Tunneling()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("1: Window");
+        }
+
+        private void Grid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("2: Grid");
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("3: Button");
+        }
+
+        private void Canvas_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("4: Canvas");
+        }
+
+        private void Ellipse_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("5: Ellipse");
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Grid Mouse Down");
+        }
+    }
+}
